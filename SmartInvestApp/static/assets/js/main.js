dc24ee9cf175aa5eb2d11bd49194f3ef483a6988
Tiger-Foxx <donfackarthur750@gmail.com>
2024-03-27 08:20:59 +0100

    lastest

--- SmartInvestApp/static/assets/js/main.js
+++ SmartInvestApp/static/assets/js/main.js
@@ -51,3 +51,45 @@
     mainWrapper.classList.remove("active");
   });
 })();
+
+(function() {
+  "use strict";
+
+  function copyToClipboard(elem) {
+    var target = elem;
+
+    // select the content
+    var currentFocus = document.activeElement;
+
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+
+    // copy the selection
+    var succeed;
+
+    try {
+      succeed = document.execCommand("copy");
+    } catch (e) {
+      console.warn(e);
+
+      succeed = false;
+    }
+
+    // Restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+      currentFocus.focus();
+    }
+
+    if (succeed) {
+      $(".copied").animate({ top: -25, opacity: 0 }, 700, function() {
+        $(this).css({ top: 0, opacity: 1 });
+      });
+    }
+
+    return succeed;
+  }
+
+  $("#copyButton, #copyTarget").on("click", function() {
+    copyToClipboard(document.getElementById("copyTarget"));
+  });
+})();
